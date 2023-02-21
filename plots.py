6e79535978915cb5cb53126a5e00a5477ab16200
authierj <authierj@julescomputer>
2023-02-21 11:44:51 -0500

    automatic figure storage with meaningfull name added

--- plots.py
+++ plots.py
@@ -5,6 +5,8 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy.stats import pearsonr as pcorr
 from matplotlib.ticker import FormatStrFormatter
 from IPython.display import set_matplotlib_formats
+import os
+
 
 set_matplotlib_formats('retina')
 
@@ -24,4 +26,8 @@ def loss_cruve(train_losses, valid_losses, args):
     ax1.set_ylabel('Loss')
     ax1.set
 
-    fig1.savefig('plots/' + args['GNN'] + '_' + args['readout'] + '_' + 'loss_curve.png' )
+    plot_path = 'plots/' + '_'.join(args['network'], args['GNN'], args['readout'], 'loss_curve.png')
+
+    if os.path.isfile(plot_path):
+        os.remove(plot_path)
+    fig1.savefig(plot_path)
