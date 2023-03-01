68b59853d54f51c9bd6b2cb5ce5bfe93f79407ef
authierj <authierj@julescomputer>
2023-03-01 17:18:02 -0500

    default_args function added to utils_JA.py

--- utils_JA.py
+++ utils_JA.py
@@ -493,3 +493,42 @@ def xgraph_xflatten(x_graph, batch_size, first_node=False):
     xNN_3d = torch.transpose(graph_3d, 1, 2)
     xNN = torch.flatten(xNN_3d, 1, 2)
     return xNN
+
+
+
+def default_args():
+    defaults = {}
+
+    defaults['network'] = 'node4'
+    defaults['epochs'] = 300
+    defaults['batchSize'] = 200
+    defaults['lr'] = 1e-4  # NN learning rate
+    defaults['GNN'] = "GCN"
+    defaults["readout"] = "GlobalMLP"
+    defaults["numLayers"] = 2
+    defaults["hiddenFeatures"] = 5
+    defaults["outputFeatures"] = 5
+    defaults['softWeight'] = 100  # this is lambda_g in the paper
+    # whether lambda_g is time-varying or not
+    defaults['useAdaptiveWeight'] = False
+    # whether lambda_g is scalar, or vector for each constraint
+    defaults['useVectorWeight'] = False
+    defaults['adaptiveWeightLr'] = 1e-2
+    # use 100 if useCompl=False
+
+    # defaults['softWeightEqFrac'] = 0.5
+    defaults['useCompl'] = True
+    defaults['useTrainCorr'] = False
+    defaults['useTestCorr'] = False
+    defaults['corrTrainSteps'] = 5  # 20 for train correction
+    # 5 for with train correction, 500 for without train correction
+    defaults['corrTestMaxSteps'] = 5
+    defaults['corrEps'] = 1e-3
+    defaults['corrLr'] = 1e-4  # 1e-4  # use 1e-5 if useCompl=False
+    defaults['corrMomentum'] = 0.5  # 0.5
+    defaults['saveAllStats'] = True
+    defaults['resultsSaveFreq'] = 50
+    defaults['saveModel'] = False
+    defaults['dropout'] = 0.5
+
+    return defaults
